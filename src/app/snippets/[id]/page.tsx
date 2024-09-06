135be2e86759ae67d887517c28a57f2355d93948
Zafeer Hafeez <contact@zafeer.pk>
2024-09-07 01:55:02 +0500

    feat: add update and delete actions

--- src/app/snippets/[id]/page.tsx
+++ src/app/snippets/[id]/page.tsx
@@ -1,5 +1,6 @@
 import { db } from "@/db";
 import { notFound } from "next/navigation";
+import Link from "next/link";
 
 interface SnippetPageProps {
   params: {
@@ -19,8 +20,36 @@ export default async function SnippetPage({ params }: SnippetPageProps) {
   }
 
   return (
-    <div>
-      <h1>{snippet.title}</h1>
+    <div className="max-w-2xl max-md:max-w-md mx-auto flex flex-col justify-center h-screen gap-2">
+      <h1 className="text-4xl font-bold mb-12 text-center">Snippet</h1>
+      <div className="flex justify-between items-center">
+        <h1 className="text-xl font-bold mb-4">{snippet.title}</h1>
+        <div className="flex gap-2">
+          <Link
+            href={`/snippets/${snippet.id}/edit`}
+            className="bg-blue-500 hover:bg-blue-600 text-white px-4 py-2 rounded-md"
+          >
+            Edit
+          </Link>
+          <Link
+            href={`/snippets/${snippet.id}/delete`}
+            className="bg-red-500 hover:bg-red-600 text-white px-4 py-2 rounded-md"
+          >
+            Delete
+          </Link>
+        </div>
+      </div>
+      {/* show code block with line numbers */}
+      <div className="bg-gray-100 p-4 rounded-md">
+        {snippet.code.split("\n").map((line, index) => (
+          <pre key={index}>
+            <span className="select-none text-gray-500 border-r border-gray-300 pr-2 py-1">
+              {index + 1}
+            </span>{" "}
+            {line}
+          </pre>
+        ))}
+      </div>
     </div>
   );
 }
